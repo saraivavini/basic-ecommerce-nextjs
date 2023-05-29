9a72763d6d1f32f075aa283d63e7e77e4e41e572
josevsaraiva@gmail.com <zeviniciusbs@pop-os.localdomain>
2023-05-28 23:35:03 -0300

    refactor: Redirect to products

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <div className="flex px-6 py-4">
-      <p className="font-bold text-lg">Minishop</p>
+      <Link href="/products">
+        <p className="font-bold text-lg">Minishop</p>
+      </Link>
     </div>
   )
 }
